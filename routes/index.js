108e5ea6990a87e58426d6cc1ec22fd7a74fdad8
Razzein <razzein@hotmail.com>
2015-07-28 19:34:05 +0200

    Autoload

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,14 @@ var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
+// Pagina de entrada (home page)
 
 router.get('/', function(req, res) {
   res.render('index', { title: 'QUIZ' });
 });
+
+// Autoload de comandos con :quizId
+router.param ('quizId', quizController.load);  // autoload :quizId
 
 // Definicion de rutas de /quizes
 router.get('/quizes', quizController.index);

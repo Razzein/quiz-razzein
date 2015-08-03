3a07c59c59bd3df8112c589029a569b58ce4bbd3
Razzein <razzein@hotmail.com>
2015-08-03 20:17:18 +0200

    Ejercicio P2P modulo 8

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -44,7 +44,7 @@ exports.answer = function (req, res) {
 // GET /quizes/new
 exports.new = function (req, res) {
   var quiz = models.Quiz.build (  //Crea objeto quiz
-    {pregunta: "Pregunta", respuesta: "Respuesta"}
+    {pregunta: "Pregunta", respuesta: "Respuesta", tema:"Tema"}
   );
   res.render('quizes/new', {quiz: quiz, errors: []});
 };
@@ -61,7 +61,7 @@ exports.create = function (req, res) {
         res.render('quizes/new', {quiz: quiz, errors: err.errors});
       } else {
         quiz // save: guarda en DB los campos pregunta y respuesta de quiz
-        .save({fields: ["pregunta", "respuesta"]})
+        .save({fields: ["pregunta", "respuesta", "tema"]})
         .then(function() {res.redirect('/quizes')})
       }       // res.redirect:  Redireccion HTTP a lista de preguntas
     }
@@ -79,6 +79,7 @@ exports.edit = function (req, res) {
 exports.update = function (req, res) {
   req.quiz.pregunta = req.body.quiz.pregunta;
   req.quiz.respuesta = req.body.quiz.respuesta;
+  req.quiz.tema = req.body.quiz.tema;
 
   req.quiz
   .validate()
@@ -88,7 +89,7 @@ exports.update = function (req, res) {
         res.render('quizes/edit', {quiz: req.quiz, errors:err.errors});
       } else {
         req.quiz
-        .save ( {fields: ["pregunta", "respuesta"]})
+        .save ( {fields: ["pregunta", "respuesta", "tema"]})
         .then ( function(){ res.redirect('/quizes');});
       }     // Redireccion HTTP a la lista de preguntas (URL relativo)
     }
--- models/quiz.js
+++ models/quiz.js
@@ -10,6 +10,10 @@ module.exports = function (sequelize, DataTypes) {
       respuesta: {
         type: DataTypes.STRING,
         validate: {notEmpty: {msg: "--> Falta Respuesta"}}
+      },
+      tema: {
+        type: DataTypes.ENUM('otro', 'humanidades', 'ocio', 'ciencia', 'tecnologia'),
+        validate: {notEmpty: {msg: '--> Falta Categoria'}}
       }
     }
   );
